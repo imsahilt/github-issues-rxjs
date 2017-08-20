d30ad75e4f37b98171a6665d21196e38ec963fa7
Sahil Taneja <sahil@taskbob.com>
2017-08-20 17:42:29 +0530

    api fixes

--- app.js
+++ app.js
@@ -14,33 +14,35 @@ app.get('/', function (req, res) {
 
 app.get('/repo/:author/:repo/get-issues/:page', function (req, res) {
     cache_key = req.params.author + ":" + req.params.repo + ":" + req.params.page
-    client.get(cache_key,function (error, entry) {
+    var cache_response;
+    cache_response = client.get(cache_key,function (error, entry) {
       if ( error ){
         console.log("key: "+ cache_key +" error occured.");
       }
       //handle json parse execptions.
       if(entry.length>0){
-        console.log("key: "+ cache_key +"found and serving from cache..")
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.parse(entry));
-        console.log("key: "+ cache_key +"found and served from cache..")
+        console.log("key: "+ cache_key +" found and served from cache..")
       }
     });
 
-    var request = require('request');
-    var options = {
-        url: 'https://api.github.com/repos/' + req.params.author+ '/'+ req.params.repo+ '/issues?page='+ req.params.page,
-        headers: {
-            'User-Agent': 'request'
-            }
-        };
-    request(options, function (error, response, body) {
-        //handle status code.
-        client.set(cache_key, JSON.stringify(body));
-        console.log("key: "+ cache_key +"added to cache cache..")
-        res.setHeader('Content-Type', 'application/json');
-        res.send(body);
-    })
+    if(!cache_response){
+        var request = require('request');
+        var options = {
+            url: 'https://api.github.com/repos/' + req.params.author+ '/'+ req.params.repo+ '/issues?page='+ req.params.page,
+            headers: {
+                'User-Agent': 'request'
+                }
+            };
+        request(options, function (error, response, body) {
+            //handle status code.
+            client.set(cache_key, JSON.stringify(body));
+            console.log("key: "+ cache_key +"added to cache cache..")
+            res.setHeader('Content-Type', 'application/json');
+            res.send(body);
+        })
+    }
 })
 
 app.listen(3000, function () {
